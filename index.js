476e7cc5cd076f7088b6a5ea1b6029a6852be09c
Nicolas Padiou <npadiou@gmail.com>
2023-06-18 17:21:01 +0200

    fix create profile

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ express()
       try {
         const client = await pool.connect();
         await client.query(
-          'INSERT salesforce.contact (lastname, firstname, phone, mobilephone, email, password__c) VALUES ($1, $2, $3, $4, $5, $6)',
+          'INSERT salesforce.contact (lastname, firstname, phone, mobilephone, email, password__c, externalid__c) VALUES ($1, $2, $3, $4, $5, $6, $5)',
           [req.body.lastname, req.body.firstname, req.body.phone, req.body.mobilephone, req.body.email, req.body.password]
         );
         client.release();
